1dd31d59a4c27e3768b130876c1f5080b591e0be
Sriteja Thuraka <sritejathuraka@sritejas-mbp.fios-router.home>
2017-01-09 17:41:44 -0600

    Done with adddetailview controller

--- MakeANote/AddDetailViewController.swift
+++ MakeANote/AddDetailViewController.swift
@@ -7,13 +7,43 @@
 //
 
 import UIKit
+import CoreData
 
 class AddDetailViewController: UIViewController {
-
-    override func viewDidLoad() {
+    
+    
+    @IBOutlet weak var nameTextfield: UITextField!
+    @IBOutlet weak var contactTextfield: UITextField!
+    @IBOutlet weak var companyTextfield: UITextField!
+    @IBOutlet weak var designationTextfield: UITextField!
+    @IBOutlet weak var meetinglocationTextfield: UITextField!
+    
+   override func viewDidLoad() {
         super.viewDidLoad()
+    
+    let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+    
+    view.addGestureRecognizer(tap)
 
-        // Do any additional setup after loading the view.
+    }
+    
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+     @IBAction func saveButton(_ sender: Any) {
+        
+        if let appDelegate = (UIApplication.shared.delegate as? AppDelegate) {
+           let MakeAMeet = MakeAMeetmo(context:appDelegate.persistentContainer.viewContext)
+           MakeAMeet.name = nameTextfield.text
+           MakeAMeet.contact = contactTextfield.text
+           MakeAMeet.company = companyTextfield.text
+           MakeAMeet.desination = designationTextfield.text
+           MakeAMeet.location = meetinglocationTextfield.text
+           navigationController?.popViewController(animated: true)
+           appDelegate.saveContext()
+            
+    }
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,14 +52,5 @@ class AddDetailViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
